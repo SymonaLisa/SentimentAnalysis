43933ea2f0b4de6f7ceb7dbe7661c1d9ed58855b
Virgi Symone Jordaan <virgi.s.jordaan@gmail.com>
2025-06-11 11:46:38 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,15 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }]
+        ]
+      }
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
